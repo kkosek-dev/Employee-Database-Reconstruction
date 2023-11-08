e7864b2a78d27a12a60e6cbc0bbc0f819b628e9a
kkosek-dev <kkosek@alum.utk.edu>
2023-11-08 17:27:52 -0500

    Final Draft Update

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -1,8 +1,59 @@
-SELECT * 
-FROM "employees";
+-- List the employee number, last name, first name, sex, and salary of each employee.
+SELECT e."emp_no", e."last_name", e."first_name", e."sex", s."salary"
+FROM "employees" AS e
+INNER JOIN "salaries" AS s
+ON e."emp_no" = s."emp_no";
 
-SELECT "emp_no","last_name","first_name","sex",
- (SELECT "salary"
-  FROM "salaries"
-  WHERE "salaries"."emp_no" = "employees"."emp_no")
-  FROM employees;
+-- List the first name, last name, and hire date for the employees who were hired in 1986.
+SELECT e."first_name", e."last_name", e."hire_date"
+FROM "employees" AS e
+WHERE e."hire_date" LIKE '%1986%';
+
+-- List the manager of each department along with their department number, department name, employee number, last name, and first name.
+SELECT d."dept_no", d."dept_name", e."emp_no", e."last_name", e."first_name"
+FROM "employees" AS e
+INNER JOIN "dept_manager" AS dm
+ON e."emp_no"=dm."emp_no"
+INNER JOIN "departments" AS d
+ON dm."dept_no" = d."dept_no"
+ORDER BY e."emp_no";
+
+-- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+SELECT d."dept_no", e."emp_no", e."last_name", e."first_name", d."dept_name"
+FROM "employees" AS e
+INNER JOIN "dept_emp" AS de
+ON e."emp_no"=de."emp_no"
+INNER JOIN "departments" AS d
+ON de."dept_no" = d."dept_no"
+ORDER BY e."emp_no";
+
+-- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+SELECT "first_name", "last_name", "sex"
+FROM "employees"
+WHERE "first_name" = 'Hercules'
+AND "last_name" LIKE 'B%';
+
+-- List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT e."emp_no", e."last_name", e."first_name"
+FROM "employees" AS e
+INNER JOIN "dept_emp" AS de
+ON e."emp_no"=de."emp_no"
+INNER JOIN "departments" AS d
+ON de."dept_no" = d."dept_no"
+WHERE d."dept_name" = 'Sales';
+
+-- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e."emp_no", e."last_name", e."first_name", d."dept_name"
+FROM "employees" AS e
+INNER JOIN "dept_emp" AS de
+ON e."emp_no"=de."emp_no"
+INNER JOIN "departments" AS d
+ON de."dept_no" = d."dept_no"
+WHERE d."dept_name" = 'Sales'
+OR d."dept_name" = 'Development';
+
+-- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT "last_name", COUNT("last_name") AS "Count of last name"
+FROM "employees"
+GROUP BY "last_name" 
+ORDER BY "Count of last name" DESC;
--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -26,11 +26,11 @@ CREATE TABLE "employees" (
     "emp_no" INT   NOT NULL,
     "emp_title" VARCHAR(255)   NOT NULL,
 	FOREIGN KEY ("emp_title") REFERENCES "titles"("title_id"),
-    "birth_date" DATE   NOT NULL,
+    "birth_date" VARCHAR(255)   NOT NULL,
     "first_name" VARCHAR(255)   NOT NULL,
     "last_name" VARCHAR(255)   NOT NULL,
     "sex" VARCHAR(255)   NOT NULL,
-    "hire_date" DATE   NOT NULL,
+    "hire_date" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
